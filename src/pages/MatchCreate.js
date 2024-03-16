289fbfb614d705b64d64aa3025607e3dd2d853a1
wlsdud9973 <jyp260399@gmail.com>
2024-03-16 16:34:38 +0900

    ✨Feat : 모임 생성 퍼블리싱

--- src/pages/MatchCreate.js
+++ src/pages/MatchCreate.js
@@ -1,115 +1,104 @@
 import React, {useState} from 'react';
-import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import DropDown from '../Component/PickerComponent';
 
 const MatchCreate = () => {
   return (
     <View style={styles.screenStyle}>
-      <View
-        style={{
-          backgroundColor: '#d9d9d9',
-          width: '100%',
-          gap: 15,
-          paddingVertical: 10,
-        }}>
-        <View style={{width: '100%', gap: 10}}>
-          <Text style={{fontSize: 18}}>지역</Text>
-          <View
-            style={{
-              justifyContent: 'space-around',
-              flexDirection: 'row',
-              gap: 10,
-            }}>
-            <DropDown />
+      <ScrollView style={{width: '100%'}}>
+        <View style={{width: '100%', gap: 15, padding: 20}}>
+          <View style={{width: '100%', paddingTop: 5}} />
 
-            <TouchableOpacity
-              onPress={() => alert('구 선택')}
-              style={styles.button}>
-              <Text>구</Text>
-            </TouchableOpacity>
+          <View style={styles.columnBox}>
+            <Text style={styles.contentTitle}>지역</Text>
+            <View style={styles.rowBox}>
+              <View
+                style={[
+                  {
+                    width: '48.5%',
+                    height: 50,
+                  },
+                  styles.contentBox,
+                ]}
+              />
+              <View
+                style={[
+                  {
+                    width: '48.5%',
+                    height: 50,
+                  },
+                  styles.contentBox,
+                ]}
+              />
+            </View>
+          </View>
+          <View style={styles.columnBox}>
+            <Text style={styles.contentTitle}>모임종류</Text>
+            <View
+              style={[
+                {
+                  width: '100%',
+                  height: 50,
+                },
+                styles.contentBox,
+              ]}
+            />
+          </View>
+          <View style={styles.columnBox}>
+            <Text style={styles.contentTitle}>모임 이름</Text>
+            <TextInput
+              style={[
+                {
+                  width: '100%',
+                  height: 50,
+                },
+                styles.contentBox,
+              ]}
+              placeholder="모임 이름을 입력해주세요."
+            />
+          </View>
+          <View style={styles.columnBox}>
+            <Text style={styles.contentTitle}>모임목표</Text>
+            <TextInput
+              style={[
+                {
+                  width: '100%',
+                  height: 150,
+                },
+                styles.contentBox,
+              ]}
+              placeholder="모임목표을 설명해주세요."
+            />
+          </View>
+          <View style={styles.columnBox}>
+            <Text style={styles.contentTitle}>정원</Text>
+            <TextInput
+              style={[
+                {
+                  width: '100%',
+                  height: 50,
+                },
+                styles.contentBox,
+              ]}
+              placeholder="정원을 입력해주세요. (최대 300명)"
+            />
           </View>
         </View>
-        <View style={{width: '100%', gap: 10}}>
-          <Text style={{fontSize: 18}}>모임종류</Text>
-          <View
-            style={{
-              justifyContent: 'space-around',
-              flexDirection: 'row',
-              gap: 10,
-            }}>
-            <TouchableOpacity
-              onPress={() => alert('구 선택')}
-              style={styles.button}>
-              <Text>카테고리 선택</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={{width: '100%', gap: 10}}>
-          <Text style={{fontSize: 18}}>모임 이름</Text>
-          <View
-            style={{
-              justifyContent: 'space-around',
-              flexDirection: 'row',
-              gap: 10,
-            }}>
-            <TouchableOpacity
-              onPress={() => alert('구 선택')}
-              style={styles.button}>
-              <Text>모임 이름을 입력해주세요</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={{width: '100%', gap: 10}}>
-          <Text style={{fontSize: 18}}>모임 목표</Text>
-          <View
-            style={{
-              justifyContent: 'space-around',
-              flexDirection: 'row',
-              gap: 10,
-            }}>
-            <TouchableOpacity
-              onPress={() => alert('구 선택')}
-              style={styles.button}>
-              <Text>모임목표</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={{width: '100%', gap: 10}}>
-          <Text style={{fontSize: 18}}>정원</Text>
-          <View
-            style={{
-              justifyContent: 'space-around',
-              flexDirection: 'row',
-              gap: 10,
-            }}>
-            <TouchableOpacity
-              onPress={() => alert('구 선택')}
-              style={styles.button}>
-              <Text>정원을 입력해주세요.(최대 300명)</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-      <View
-        style={{
-          paddingHorizontal: 20,
-          paddingVertical: 10,
-          borderTopWidth: 1,
-          borderColor: '#f1f1f1',
-        }}>
-        <TouchableOpacity
-          style={{
-            backgroundColor: 'rgba(255, 206, 79, 1)',
-            borderRadius: 15,
-            height: 56,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onPress={() => alert('button눌렀엉')}>
-          <Text>모임 만들기</Text>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => alert('button눌렀엉')}>
+        <Text style={styles.buttonText}>모임 만들기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -120,17 +109,46 @@ const styles = StyleSheet.create({
   screenStyle: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 20,
-    gap: 30,
     justifyContent: 'space-between',
   },
   button: {
-    height: 50,
+    backgroundColor: 'rgba(255, 206, 79, 1)',
+    borderRadius: 15,
+    height: 56,
+    alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  rowBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  columnBox: {
+    flexDirection: 'column',
+    gap: 10,
+  },
+  contentTitle: {
+    fontSize: 15,
+    fontWeight: '600',
+  },
+  contentText: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: 'rgba(187, 187, 187, 1)',
+  },
+  contentBox: {
+    borderColor: 'rgba(221, 221, 221, 1)',
     borderWidth: 1,
-    borderRadius: 5,
-    borderColor: 'gray',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 16,
   },
 });
